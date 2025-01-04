d34254714795d2a565ebe134db361d24451656a3
Neelam123K <153060910+Neelam123K@users.noreply.github.com>
2025-01-04 10:34:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function highlight() {
     //Write your code here
+	const boldWords = document.querySelectorAll('strong');
+
+  boldWords.forEach((word) => {
+    word.style.color = 'green';
+  });
 
 
 }
@@ -7,6 +12,11 @@ function highlight() {
 
 function return_normal() {
     //Write your code here
+	 const boldWords = document.querySelectorAll('strong');
+
+  boldWords.forEach((word) => {
+    word.style.color = 'black';
+  });
 
     
 }
